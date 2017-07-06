55f82378c8847fc4d2724c077b59e9ba9a94b3eb
jhony basha <jhony basha>
2017-07-06 22:02:08 +0530

    Add project files.

---
+++ ISuitePro.ERP.Digital.CxUI/ISuitePro.ERP.Digital.CxUI/App.xaml.cs
@@ -0,0 +1,24 @@
+﻿using Prism.Unity;
+using ISuitePro.ERP.Digital.CxUI.Views;
+using Xamarin.Forms;
+
+namespace ISuitePro.ERP.Digital.CxUI
+{
+    public partial class App : PrismApplication
+    {
+        public App(IPlatformInitializer initializer = null) : base(initializer) { }
+
+        protected override void OnInitialized()
+        {
+            InitializeComponent();
+
+            NavigationService.NavigateAsync("NavigationPage/MainPage?title=Hello%20from%20Xamarin.Forms");
+        }
+
+        protected override void RegisterTypes()
+        {
+            Container.RegisterTypeForNavigation<NavigationPage>();
+            Container.RegisterTypeForNavigation<MainPage>();
+        }
+    }
+}
